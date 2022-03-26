6d1e029df45ac7582137e924d6b2582d7a6f862e
Janechj <jiec3@illinois.edu>
2022-03-26 18:20:53 -0500

    review model

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,7 @@ const seedDB = async () => {
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://images.unsplash.com/photo-1507668339897-8a035aa9527d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2865&q=80',
+            image: `https://source.unsplash.com/collection/483251`,
             description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price
         })
